96d608f347ff60fc4f5271e41ecac4c65f2b5839
Murilo <murilohidekisakomura25@gmail.com>
2024-04-18 22:52:50 -0300

    Aula 07

--- app/src/main/java/com/example/mysavings/MainActivity.java
+++ app/src/main/java/com/example/mysavings/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.mysavings;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -25,12 +34,15 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private EditText TransactionEditAmount;
+    private EditText TransactionEditTextDescription;
     private Spinner spinnerPriority;
     private Button transactionAddButtonAdd;
+    private Button buttonSendTransactions;
     private RecyclerView recyclerViewTransactions;
-    private EditText TransactionEditTextDescription;
     private LinearLayout linearLayoutIncomes;
-    private TaskAdapter taskAdapter;
+    private Button buttonFoto;
+    private TransactionAdapter transactionAdapter;
+    private ImageView imageView;
     private List<Transaction> transactions = new ArrayList<>();
 
     @Override
@@ -44,11 +56,13 @@ public class MainActivity extends AppCompatActivity {
         transactionAddButtonAdd = findViewById(R.id.buttonAddIncome);
         linearLayoutIncomes = findViewById(R.id.linearLayoutIncomes);
         recyclerViewTransactions = findViewById(R.id.recyclerViewIncomes);
-
+        buttonSendTransactions = findViewById(R.id.buttonSendTransactions);
+        imageView = findViewById(R.id.imageView);
+        buttonFoto = findViewById(R.id.foto);
 
         recyclerViewTransactions.setLayoutManager(new LinearLayoutManager(this));
-        taskAdapter = new TaskAdapter(transactions);
-        recyclerViewTransactions.setAdapter(taskAdapter);
+        transactionAdapter = new TransactionAdapter(transactions);
+        recyclerViewTransactions.setAdapter(transactionAdapter);
 
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -71,7 +85,7 @@ public class MainActivity extends AppCompatActivity {
                     transaction.setValue(taskValue);
 
                     transactions.add(transaction);
-                    taskAdapter.notifyDataSetChanged();
+                    transactionAdapter.notifyDataSetChanged();
 
                     TransactionEditAmount.setText("");
                     TransactionEditTextDescription.setText("");
@@ -80,15 +94,102 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        buttonSendTransactions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendTasksByEmail();
+            }
+        });
+
+        buttonFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tirarFoto(view);
+            }
+        });
+
+        buttonSendTransactions.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                sendTasksByEmail();
+            }
+        });
+    }
+
+    ActivityResultLauncher<Intent> fotografarLauncher = registerForActivityResult(
+            /*Aqui, você está criando um novo contrato de resultado de atividade usando
+            ActivityResultContracts.StartActivityForResult(). Esse contrato é usado para iniciar uma atividade e receber um resultado.*/
+            new ActivityResultContracts.StartActivityForResult(),
+
+            /*
+            Aqui, você está definindo um callback para lidar com o resultado da atividade. Este callback é chamado quando a atividade é concluída e retorna um resultado.
+             */
+            new ActivityResultCallback<ActivityResult>() {
+                /*
+                Aqui, você está implementando o método onActivityResult do ActivityResultCallback, que é chamado
+                quando a atividade é concluída. O parâmetro result contém o resultado da atividade.
+                 */
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    /*
+                    Aqui, você está verificando se a atividade foi concluída com sucesso. O código Activity.RESULT_OK indica que a atividade foi concluída com sucesso.
+                     */
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        //Aqui, você está obtendo os dados da intent da atividade concluída.
+                        Intent data = result.getData();
+                        //Aqui, você está obtendo os extras (dados adicionais) da intent, que podem incluir a imagem capturada pela câmera.
+                        Bundle extras = data.getExtras();
+                        //Aqui, você está obtendo a imagem capturada da intent usando a chave "data" e convertendo-a em um objeto Bitmap.
+                        Bitmap imageBitmap = (Bitmap) extras.get("data");
+                        imageView.setImageBitmap(imageBitmap);
+                    }
+                }
+            });
+
+    // Este é o método tirarFoto que é chamado quando o usuário clica em um botão para tirar uma foto.
+    public void tirarFoto(View view) {
+        //Aqui, você está criando uma intent para capturar uma imagem usando a câmera do dispositivo.
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Aqui, você está verificando se há uma atividade de câmera disponível para lidar com a intent. Se houver, o código dentro do bloco if será executado.
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //Aqui, você está iniciando a atividade de captura de imagem usando o FotografarLauncher, que está configurado para lidar com o resultado da atividade.
+            fotografarLauncher.launch(takePictureIntent);
+        }
+    }
+    private void sendTasksByEmail() {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        /*
+        Outros tipos possiveis são, por exemplo:
+        message/rfc822: Para enviar um email com formato MIME, que pode incluir anexos e formatação avançada.
+        text/html: Para enviar um email com conteúdo HTML, permitindo formatação avançada e elementos interativos.
+        image/jpeg, image/png, etc.: Para enviar imagens como anexos no email.
+        application/pdf, application/msword, etc.: Para enviar arquivos PDF, documentos do Word, e
+        outros tipos de arquivos como anexos.
+         */
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Transaction List");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, generateTasksText());
+        startActivity(Intent.createChooser(emailIntent, "Send Transaction List"));
+    }
+
+    private String generateTasksText() {
+        StringBuilder sb = new StringBuilder();
+        for (Transaction transaction : transactions) {
+            sb.append("Description: ").append(transaction.getDescription()).append("\n");
+            sb.append("Value: ").append(transaction.getValue()).append("\n");
+            sb.append("Type: ").append(transaction.getType()).append("\n");
+            //sb.append("Concluída: ").append(transaction.isCompleted() ? "Sim" : "Não").append("\n\n");
+        }
+        return sb.toString();
     }
 
     // Classe interna para o adaptador do RecyclerView
-    private class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.TaskViewHolder> {
+    private class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.TransactionViewHolder> {
 
         private List<Transaction> transactions;
 
         // Construtor que recebe a lista de tarefas
-        public TaskAdapter(List<Transaction> transactions) {
+        public TransactionAdapter(List<Transaction> transactions) {
             this.transactions = transactions;
         }
 
@@ -101,7 +202,7 @@ public class MainActivity extends AppCompatActivity {
          será inserida após a criação, e viewType, que é o tipo de visualização, caso o RecyclerView
          tenha vários tipos de itens de lista.
          */
-        public TaskViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public TransactionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
             /*
             Aqui, estamos inflando o layout task_item.xml para criar a visualização do item da lista.
             O LayoutInflater é usado para inflar layouts XML em objetos de visualização reais. parent.getContext()
@@ -117,7 +218,7 @@ public class MainActivity extends AppCompatActivity {
             sem precisar criar manualmente cada elemento de interface no código Java.
              */
             View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.transaction_item, parent, false);
-            return new TaskViewHolder(view);
+            return new TransactionViewHolder(view);
         }
 
         // Método que atualiza o conteúdo das visualizações com base nos dados da tarefa
@@ -127,7 +228,7 @@ public class MainActivity extends AppCompatActivity {
         específico. Ele recebe dois parâmetros: holder, que é a instância do TaskViewHolder que contém
         os elementos de interface do item da lista, e position, que é a posição da tarefa na lista de tarefas.
          */
-        public void onBindViewHolder(@NonNull TaskViewHolder holder, int position) {
+        public void onBindViewHolder(@NonNull TransactionViewHolder holder, int position) {
             /*
             Aqui, estamos obtendo a tarefa na posição especificada da lista de tarefas. A lista de
             tarefas (tasks) é a fonte de dados que contém todas as tarefas a serem exibidas na lista.
@@ -149,13 +250,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         // Classe interna que representa cada item de tarefa na lista
-        public class TaskViewHolder extends RecyclerView.ViewHolder {
+        public class TransactionViewHolder extends RecyclerView.ViewHolder {
             private TextView textViewDescription;
             private TextView textViewPriority;
             private TextView textViewValue;
 
             // Construtor que recebe a visualização do item de tarefa
-            public TaskViewHolder(@NonNull View itemView) {
+            public TransactionViewHolder(@NonNull View itemView) {
                 super(itemView);
                 // Inicializa os elementos de interface do item de tarefa
                 textViewPriority = itemView.findViewById(R.id.textViewPriority);
